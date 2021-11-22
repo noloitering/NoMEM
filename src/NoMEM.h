d18de0ed5497ea4d2f414c640e66a94e322f2c49
Eric Barrett <neverloiter@gmail.com>
2021-11-22 01:08:03 -0330

    Add Directory Paths
    Add Config object which contains paths to directories for assets
    Add Adder functions that don't require a path
    Add to test

--- src/NoMEM.h
+++ src/NoMEM.h
@@ -27,9 +27,77 @@ namespace NoMEM
 	typedef std::unordered_map< std::string, std::shared_ptr< Texture2D > > TextureMap;
 	typedef std::unordered_map< std::string, std::shared_ptr< Anim > > AnimMap;
 	typedef std::unordered_map< std::string, std::shared_ptr< Sprite > > SpriteMap;
-	
 	typedef std::tuple< FontMap, TextureMap, AnimMap, SpriteMap > Assets;
-
+	
+	class Config
+	{
+	private:
+		friend class MEMManager;
+		std::unordered_map< std::string, std::string > custom;
+	public:
+		Config() {}
+		Config(std::string basePath)
+		{
+			if ( basePath.back() != '/' )
+			{
+				basePath = basePath + "/";
+			}
+			textureDir = basePath + "textures/";
+			spriteDir = basePath + "textures/sprites/";
+			fontDir = basePath + "fonts/";
+		}
+		Config(std::string texturePath, std::string spritePath, std::string fontPath) 
+		{
+			if ( texturePath.back() != '/' )
+			{
+				texturePath += "/";
+			}
+			if ( spritePath.back() != '/' )
+			{
+				spritePath += "/";
+			}
+			if ( fontPath.back() != '/' )
+			{
+				fontPath += "/";
+			}
+			textureDir = texturePath;
+			spriteDir = spritePath;
+			fontDir = fontPath;
+		}
+		std::string textureDir = "../assets/textures/";
+		std::string spriteDir = "../assets/textures/sprites/";
+		std::string fontDir = "../assets/fonts/";
+		
+		bool has(const std::string& key)
+		{
+			auto found = custom.find(key);
+			if ( found != custom.end() )
+			{
+				return true;
+			}
+			
+			return false;
+		}
+		
+		std::string get(const std::string& key)
+		{
+			
+			return custom.at(key);
+		}
+		
+		std::unordered_map< std::string, std::string >::iterator find(const std::string& key)
+		{
+			
+			return custom.find(key);
+		}
+		
+		std::unordered_map< std::string, std::string >::iterator customEnd()
+		{
+			
+			return custom.end();
+		}
+	};
+	
 	class MEMManager
 	{
 	private:
@@ -37,6 +105,13 @@ namespace NoMEM
 		Assets assets;
 	public:
 		MEMManager() {}
+		template <typename... Args>
+		MEMManager(Args&&... CArgs)
+		{
+			conf = Config(std::forward<Args>(CArgs)...);
+		}
+		
+		Config conf;
 		Assets getAll()
 		{
 			
@@ -86,6 +161,7 @@ namespace NoMEM
 				FontMap fontMap = getAll< Font >();
 				fontMap[name] = newFont;
 				assets = std::make_tuple(fontMap, getAll< Texture2D >(), getAll< Anim >(), getAll< Sprite >());
+				conf.custom[name] = path;
 				
 				return newFont;
 			}
@@ -97,6 +173,39 @@ namespace NoMEM
 			return nullptr;
 		}
 		
+		std::shared_ptr< Font > addFont(const std::string& name)
+		{
+			std::string path = conf.fontDir + name;
+			if ( FileExists(path.c_str()) )
+			{
+				
+			}
+			else if ( FileExists(((path + ".ttf").c_str())) )
+			{
+				path += ".ttf";
+			}
+			else if ( FileExists(((path + ".fnt").c_str())) )
+			{
+				path += ".fnt";
+			}
+			else if ( FileExists(((path + ".png").c_str())) )
+			{
+				path += ".png";
+			}
+			else
+			{
+				std::cerr << "could not load font: " << path << std::endl;
+				
+				return nullptr;
+			}
+			std::shared_ptr< Font > newFont = std::make_shared< Font >(LoadFont(path.c_str()));
+			FontMap fontMap = getAll< Font >();
+			fontMap[name] = newFont;
+			assets = std::make_tuple(fontMap, getAll< Texture2D >(), getAll< Anim >(), getAll< Sprite >());
+				
+			return newFont;
+		}
+		
 		std::shared_ptr< Texture2D > addTexture(const std::string& name, const std::string& path)
 		{
 			if ( FileExists(path.c_str()) )
@@ -105,6 +214,7 @@ namespace NoMEM
 				TextureMap textureMap = getAll< Texture2D >();
 				textureMap[name] = newTexture;
 				assets = std::make_tuple(getAll< Font >(), textureMap, getAll< Anim >(), getAll< Sprite >());
+				conf.custom[name] = path;
 				
 				return newTexture;
 			}
@@ -116,6 +226,43 @@ namespace NoMEM
 			return nullptr;
 		}
 		
+		std::shared_ptr< Texture2D > addTexture(const std::string& name)
+		{
+			std::string path = conf.textureDir + name;
+			if ( FileExists(path.c_str()) )
+			{
+				
+			}
+			else if ( FileExists(((path + ".png").c_str())) )
+			{
+				path += ".png";
+			}
+			else if ( FileExists(((path + ".gif").c_str())) )
+			{
+				path += ".gif";
+			}
+			else if ( FileExists(((path + ".hdr").c_str())) )
+			{
+				path += ".hdr";
+			}
+			else if ( FileExists(((path + ".dds").c_str())) )
+			{
+				path += ".dds";
+			}
+			else
+			{
+				std::cerr << "could not load texture: " << path << std::endl;
+				
+				return nullptr;
+			}
+			std::shared_ptr< Texture2D > newTexture = std::make_shared< Texture2D >(LoadTexture(path.c_str()));
+			TextureMap textureMap = getAll< Texture2D >();
+			textureMap[name] = newTexture;
+			assets = std::make_tuple(getAll< Font >(), textureMap, getAll< Anim >(), getAll< Sprite >());
+				
+			return newTexture;
+		}
+		
 		std::shared_ptr< Sprite > addSprite(const std::string& name, const std::string& path, int frames = 1)
 		{
 			if ( FileExists(path.c_str()) )
@@ -124,6 +271,7 @@ namespace NoMEM
 				SpriteMap spriteMap = getAll< Sprite >();
 				spriteMap[name] = newSprite;
 				assets = std::make_tuple(getAll< Font >(), getAll< Texture2D >(), getAll< Anim >(), spriteMap);
+				conf.custom[name] = path;
 				
 				return newSprite;
 			}
@@ -133,6 +281,43 @@ namespace NoMEM
 			}
 			
 			return nullptr;
+		}
+		
+		std::shared_ptr< Sprite > addSprite(const std::string& name, int frames = 1)
+		{
+			std::string path = conf.spriteDir + name;
+			if ( FileExists(path.c_str()) )
+			{
+				
+			}
+			else if ( FileExists(((path + ".png").c_str())) )
+			{
+				path += ".png";
+			}
+			else if ( FileExists(((path + ".gif").c_str())) )
+			{
+				path += ".gif";
+			}
+			else if ( FileExists(((path + ".hdr").c_str())) )
+			{
+				path += ".hdr";
+			}
+			else if ( FileExists(((path + ".dds").c_str())) )
+			{
+				path += ".dds";
+			}
+			else
+			{
+				std::cerr << "could not load texture: " << path << std::endl;
+				
+				return nullptr;
+			}
+			std::shared_ptr< Sprite > newSprite = std::make_shared< Sprite >((Sprite){LoadTexture(path.c_str()), frames});
+			SpriteMap spriteMap = getAll< Sprite >();
+			spriteMap[name] = newSprite;
+			assets = std::make_tuple(getAll< Font >(), getAll< Texture2D >(), getAll< Anim >(), spriteMap);
+				
+			return newSprite;
 		}
 		
 		// TODO: replace shared_ptrs with nullptr??
